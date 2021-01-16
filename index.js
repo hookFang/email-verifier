97c90de6e4f40c541dc3fec24637cde55cb5dbc5
Edwin Christie <edwinchristie100@gmail.com>
2021-01-15 22:46:45 -0500

    Code updated

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var EmailVerifier = {
   DEFAULT_REGEX:
-    "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+(?:[A-Z]{2}|com",
-  UPDATED_REGEX: "",
+  "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+(?:[A-Z]{2}|com",
+  DEFAULT_TOP_LEVEL_DOMAIN_REGEX: "",  
+  UPDATED_REGEX_TOP_LEVEL_DOMAIN: "",
 };
 
 EmailVerifier.verify = function (mailID) {
   //Initial Regex String
   var regexString = this.DEFAULT_REGEX;
   //Adding options to the string
-  if (!this.UPDATED_REGEX) {
+  if (!this.UPDATED_REGEX_TOP_LEVEL_DOMAIN) {
     regexString += ")";
   } else {
-    regexString += this.UPDATED_REGEX;
-    console.log(regexString);
+    regexString += this.UPDATED_REGEX_TOP_LEVEL_DOMAIN;
   }
 
   //Create the Regex with the Properties
@@ -26,15 +26,16 @@ EmailVerifier.verify = function (mailID) {
 };
 
 EmailVerifier.domainsValid = function ({ domainsValid }) {
+  if(this.UPDATED_REGEX_TOP_LEVEL_DOMAIN) {
+    this.UPDATED_REGEX_TOP_LEVEL_DOMAIN = this.DEFAULT_TOP_LEVEL_DOMAIN_REGEX;
+  }
   if (!domainsValid) {
-    this.UPDATED_REGEX += ")";
+    this.UPDATED_REGEX_TOP_LEVEL_DOMAIN += ")";
   } else {
     domainsValid.forEach((value) => {
-      console.log(value);
-      this.UPDATED_REGEX += "|" + value;
+      this.UPDATED_REGEX_TOP_LEVEL_DOMAIN += "|" + value;
     });
-    this.UPDATED_REGEX += ")";
-    console.log(this.UPDATED_REGEX);
+    this.UPDATED_REGEX_TOP_LEVEL_DOMAIN += ")";
   }
 };
 
